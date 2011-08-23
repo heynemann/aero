d8aceb412d6a399807f4c7116a90e2dab819f7cb
Bernardo Heynemann <heynemann@gmail.com>
2011-08-23 19:26:59 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pyVows testing engine
+# https://github.com/heynemann/pyvows
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+from setuptools import setup
+from aero.version import __version__
+
+setup(
+    name = 'aero',
+    version = '.'.join([str(item) for item in __version__]),
+    description = "aero adds django-like apps support to tornado and automates common actions",
+    long_description = open('README.rst').read(),
+    keywords = 'tornado aero app django',
+    author = 'Bernardo Heynemann',
+    author_email = 'heynemann@gmail.com',
+    #Contributors
+    #contributor = 'Rafael Carício',
+    #contributor_email = 'rafael@caricio.com',
+    url = 'http://heynemann.github.com/aero/',
+    license = 'MIT',
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: MacOS',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.6',
+    ],
+    packages = ['aero'],
+    package_dir = {"aero": "aero"},
+
+    install_requires = open('requirements.txt').read().split('\n'),
+    tests_require = open('test_requirements.txt').read().split('\n'),
+
+    entry_points = {
+        'console_scripts': [
+            'aero = aero.console:main'
+        ],
+    },
+
+)
+
+
