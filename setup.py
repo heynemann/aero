f1cdea99270b2ebd5ac1e253387bc8ed5541e51b
Rafael Caricio <rafael.jacinto@gmail.com>
2011-08-24 15:22:42 -0300

    Updating the setup.py.

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
+from os.path import exists
 from setuptools import setup
+
 from aero.version import __version__
+
+read_file = lambda filename: exists(filename) and open(filename).read() or ''
 
 setup(
     name = 'aero',
     version = '.'.join([str(item) for item in __version__]) + 'a',
     description = "aero adds django-like apps support to tornado and automates common actions",
-    long_description = open('README.rst').read(),
+    long_description = read_file('README.rst'),
     keywords = 'tornado aero app django',
     author = 'Bernardo Heynemann',
     author_email = 'heynemann@gmail.com',
@@ -32,8 +36,10 @@ setup(
     packages = ['aero'],
     package_dir = {"aero": "aero"},
 
-    install_requires = open('requirements.txt').read().split('\n'),
-    tests_require = open('test_requirements.txt').read().split('\n'),
+    install_requires = ['tornado',
+                        'cyrusbus==0.1.0'],
+    tests_require = ['coverage',
+                     'tornado-pyvows==0.3.0'],
 
     entry_points = {
         'console_scripts': [
@@ -42,5 +48,3 @@ setup(
     },
 
 )
-
-
